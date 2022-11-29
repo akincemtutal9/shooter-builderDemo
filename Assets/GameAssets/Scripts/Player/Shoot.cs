a0a7929b883e84290b24a5b1e04f8626b878fd44
akincemtutal9 <akintutall@gmail.com>
2022-11-29 14:41:25 +0300

    Shoot Speed

--- Assets/GameAssets/Scripts/Player/Shoot.cs
+++ Assets/GameAssets/Scripts/Player/Shoot.cs
@@ -7,6 +7,8 @@ public class Shoot : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform bulletSpawnPoint;
     public float bulletSpeed;
+    public float nextFireCheck;
+    public float nextFireRate;
     void Start()
     {
         
@@ -15,10 +17,13 @@ public class Shoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        
+        if (Time.time > nextFireCheck)
         {
+            nextFireCheck = Time.time + nextFireRate;
             var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
             bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
+            
         }
     }
 }
